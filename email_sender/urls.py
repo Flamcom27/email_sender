5b01c77d7242d25d3f8b2951515b0248d64b242e
Flamcom27 <27flamcom06@gmail.com>
2022-01-23 04:04:00 +0300

    django-environ is implemented

--- email_sender/urls.py
+++ email_sender/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import send_email
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', send_email, name='send_email')
 ]
